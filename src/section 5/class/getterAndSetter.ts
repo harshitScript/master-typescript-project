fcfa6e8703d088757f69620fe6028e634867563e
harshitScript <www.harshitbhawsar777@gmail.com>
2024-03-27 22:57:00 +0530

    [Class]: getter and setter methods

---
+++ src/section 5/class/getterAndSetter.ts
@@ -0,0 +1,26 @@
+//? The get (getter) and set (setter) methods in classes.
+//* These are the methods that can be accessed as properties in the classes.
+
+class School {
+    get schoolName() {
+        return this.name;
+    }
+
+    set schoolRevenue(revenue: string) {
+        this.revenue = revenue;
+    }
+
+    constructor(public name: string, public phone: number, private revenue: string) { };
+
+    getSchoolRevenue() {
+        return this.revenue;
+    }
+}
+
+const subhash = new School('Subhash English School', 242401, '3 lac');
+
+console.log('The school name => ', subhash.schoolName);
+
+subhash.schoolRevenue = '10 lac';
+
+console.log('The school revenue => ', subhash.getSchoolRevenue());
