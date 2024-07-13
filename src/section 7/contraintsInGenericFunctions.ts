435e989cecfdeeeccad696dd97576659888d8179
Harshit Bhawsar <harshit.bhawsar@taqtics.co>
2024-07-13 06:31:51 +0000

    [Generics]: keyof constraint in generics

--- src/section 7/contraintsInGenericFunctions.ts
+++ src/section 7/contraintsInGenericFunctions.ts
@@ -12,3 +12,13 @@ function mergeObject<T extends object, U extends object>(obj1: T, obj2: U) {
     return { ...obj1, ...obj2 }
 }
 console.log('The generic with constarints => ', mergeObject({ name: 'Harshit' }, { age: 24 })) // output {name: 'Harshit', age : 24}
+
+//* The "keyof" constaint in generic
+// It defines that the passed value must be the key of the object.
+
+function extractTheValue<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key];
+}
+//! extractTheValue({}, 'name') Error (key is not present in object.)
+extractTheValue({ name: 'Harshit' }, 'name')
+extractTheValue(['Harshit'], 0) //? behind the scenes array is also an object in js.
